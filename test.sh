9acc3ff3e3481f1ad926ed8ceed134cabc71893f
“Carlos <carlos@carlosguerrero.com>
2010-08-27 10:14:09 -0430

    Version 1.0 Funcional

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ ! -e /usr/share/plymouth/themes/canaima-plymouth ]
+then
+    sudo mkdir /usr/share/plymouth/themes/canaima-plymouth
+fi
+
+sudo cp * /usr/share/plymouth/themes/canaima-plymouth/
+sudo plymouth-set-default-theme canaima-plymouth
+sudo plymouthd --debug --debug-file=/tmp/plymouth-debug-out ; sudo plymouth --show-splash ; for ((I=0;I<10;I++)); do sleep 1 ; sudo plymouth --update=event$I ; done ; sudo plymouth --quit
+
